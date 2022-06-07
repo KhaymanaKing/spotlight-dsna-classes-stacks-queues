a68b8aad4fa9b0556ef0c71f5012e62ce346a5fb
Mary Martinez <mary.diana.martinez@gmail.com>
2022-06-07 16:49:28 -0700

    partially working queue

--- index.js
+++ index.js
@@ -1,34 +1,53 @@
 class Stack {
-    #list =[];
-    constructor (initialList){
-        if (initialList) this.#list = initialList;
-    }
-    push(item){
-        this.#list.push(item);
-    }
-    pop(){
-        const last = this.#list.slice(-1);
-        this.#list.pop();
-        return last[0];
-    }
-    peek(){
-        return this.#list.slice(-1)[0];
-    }
+  #list = [];
+  constructor(initialList) {
+    if (initialList) this.#list = initialList;
+  }
+  push(item) {
+    this.#list.push(item);
+  }
+  pop() {
+    const last = this.#list.slice(-1);
+    this.#list.pop();
+    return last[0];
+  }
+  peek() {
+    return this.#list.slice(-1)[0];
+  }
 }
 
-const stack = new Stack();
-stack.push('fox');
-stack.push('goose');
-stack.push('lizard');
-console.log(stack.pop()); // 'lizard'
-console.log(stack.peek()); // 'goose'
-console.log(stack.pop()); // 'goose'
-stack.push('llama');
-console.log(stack.pop()); // 'llama'
-console.log(stack.peek()); // 'fox'
-console.log(stack.pop()); // 'fox'
-console.log(stack.pop()); // null
+// const stack = new Stack();
+// stack.push('fox');
+// stack.push('goose');
+// stack.push('lizard');
+// console.log(stack.pop()); // 'lizard'
+// console.log(stack.peek()); // 'goose'
+// console.log(stack.pop()); // 'goose'
+// stack.push('llama');
+// console.log(stack.pop()); // 'llama'
+// console.log(stack.peek()); // 'fox'
+// console.log(stack.pop()); // 'fox'
+// console.log(stack.pop()); // null
 
-class Queue {}
+class Queue {
+  #list = [];
+  constructor(initialList) {
+    if (initialList) this.#list = initialList;
+  }
+  enqueue(item) {
+    this.#list.push(item);
+    // return this.#list.toString();
+  }
+  dequeue() {
+    const first = this.#list.slice[0];
+    return first;
+  }
+}
+const queue = new Queue();
+queue.enqueue('fox');
+queue.enqueue('goose');
+queue.enqueue('lizard');
+console.log(queue.dequeue()); // 'fox'
+
 
 module.exports = { Stack, Queue };
